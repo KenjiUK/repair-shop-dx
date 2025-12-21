1766acb591230c51a6f15d90f25587f55b693845
ymworkseurope <admin@ymworks.com>
2025-12-21 10:32:47 +0000

    Remove dynamic export from page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-export const dynamic = 'force-dynamic';
-
 import { useState, useMemo, useRef, useEffect } from "react";
 import useSWR from "swr";
 import { Badge } from "@/components/ui/badge";
